9165ff5197d1a6a51214f69d0e55f2b23f35d4f4
Jamesalbisa <hamsmakulit@gmail.com>
2026-01-26 08:13:58 +0000

    Add loop to allow multiple calculations in one session

--- test.py
+++ test.py
@@ -1,31 +1,35 @@
 print("Simple Calculator")
 
-# Ask the user for numbers
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+while True:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
 
-# Ask the user for an operator
-operator = input("Enter an operator (+, -, *, /): ")
+    operator = input("Enter an operator (+, -, *, /): ")
 
-# Perform the calculation
-if operator == "+":
-    result = num1 + num2
-    print("Result:", result)
-
-elif operator == "-":
-    result = num1 - num2
-    print("Result:", result)
-
-elif operator == "*":
-    result = num1 * num2
-    print("Result:", result)
-
-elif operator == "/":
-    if num2 == 0:
-        print("Error: Cannot divide by zero")
-    else:
-        result = num1 / num2
+    if operator == "+":
+        result = num1 + num2
         print("Result:", result)
 
-else:
-    print("Invalid operator")
+    elif operator == "-":
+        result = num1 - num2
+        print("Result:", result)
+
+    elif operator == "*":
+        result = num1 * num2
+        print("Result:", result)
+
+    elif operator == "/":
+        if num2 == 0:
+            print("Error: Cannot divide by zero")
+        else:
+            result = num1 / num2
+            print("Result:", result)
+
+    else:
+        print("Invalid operator")
+
+    choice = input("Do you want to calculate again? (y/n): ").lower()
+    if choice != "y":
+        print("Goodbye!")
+        break
+
